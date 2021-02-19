832b80100042ef592f99adc94a5af7c76e91a938
pankaj.sainic <pankaj.saini5@wipro.com>
2021-02-19 16:54:50 +0000

    added iam_user module to create users

--- main.tf
+++ main.tf
@@ -11,3 +11,36 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+locals {
+  env_users_table = flatten([
+    for env in var.environments : [
+      for user in var.users : {
+        env      = env
+        username = format("%s-%s", user, env)
+      }
+    ]
+  ])
+}
+
+output "env_users_table" {
+  value = local.env_users_table
+}
+
+module "iam_user" {
+  for_each                      = { for t in local.env_users_table : t.username => t }
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version                       = "3.8.0"
+  name                          = each.key
+  create_iam_access_key         = true
+  force_destroy                 = true
+  create_iam_user_login_profile = false
+  tags = {
+    env = each.value["env"]
+  }
+}
+
+output "userdetails" {
+  value = values(module.iam_user)[*]
+}
+
