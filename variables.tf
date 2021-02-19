16fa30fc8e4eb0d7e08066a33626a49ba5b8cd08
pankaj.sainic <pankaj.saini5@wipro.com>
2021-02-19 19:08:28 +0000

    added descriptions in variables

--- variables.tf
+++ variables.tf
@@ -4,11 +4,13 @@ variable "region" {
 }
 
 variable "users" {
+  description = "List of Users"
   type    = list(string)
   default = ["nevsa", "cordelia", "kriste", "darleen", "wynnie", "vonnie", "emelita", "maurita", "devinne", "breena"]
 }
 
 variable "environments" {
+  description = "List of Environments"
   type    = list(string)
   default = ["dev", "qa", "uat", "test", "prod"]
 }
